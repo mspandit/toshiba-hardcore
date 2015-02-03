d7acf240d51c7e9cb760b89a91c604fa56a7368a
Milind S. Pandit <mspandit@yahoo.com>
2015-02-03 05:37:50 -0800

    Reverting to Rails 3.1 installation.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,8 +22,9 @@ Hardcore::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  config.assets.enabled = false # Disable asset pipeline
-
   # Do not compress assets
   config.assets.compress = false
+
+  # Expands the lines which load the assets
+  config.assets.debug = true
 end
